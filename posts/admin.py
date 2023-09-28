a4673c825228c3bc814472943216071e5f407998
FerranPalmaC <fpalmcom@sirt.com>
2023-09-28 10:20:14 +0200

    PostAdmin created

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Post
 # Register your models here.
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_date', 'updated_on', 'status')
+    list_filter = ('status', )
+
+admin.site.register(Post, PostAdmin)
 
