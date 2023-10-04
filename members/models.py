23b783f045cfba079b820fa3b7a9fe47418b4484
FerranPalmaC <fpalmcom@sirt.com>
2023-10-04 08:55:34 +0200

    str method added to user

--- members/models.py
+++ members/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
 
+    def __str__(self):
+        return f"{self.username} -- {self.email}"
+
