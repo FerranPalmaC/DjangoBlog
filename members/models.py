ad3f650e3993be357c4476d7062ab047b342115e
FerranPalmaC <fpalmcom@sirt.com>
2023-10-05 09:44:19 +0200

    Username and email cant be the same

--- members/models.py
+++ members/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 from django.db import models
 # Create your models here.
 
@@ -7,4 +8,14 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return f"{self.username} -- {self.email}"
+
+    # username and email can't be the same
+    def clean(self):
+        if self.username == self.email:
+            raise ValidationError("Username and email can't be the same. Please, provide a different username/email")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
 
