eb2992378a7e381626665145c46dc5b771bbb073
FerranPalmaC <fpalmcom@sirt.com>
2023-10-06 09:27:16 +0200

    Comment class added to admin and filter by user added in post admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_date', 'updated_on', 'status')
-    list_filter = ('status', )
+    list_filter = ('status', 'author')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'publication_date', 'active')
+    list_filter = ('author', 'post', 'active')
+    
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
 
