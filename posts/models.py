c2d5cf3475668953298a029eb0e43e489f0f73fd
FerranPalmaC <fpalmcom@sirt.com>
2023-10-06 09:23:46 +0200

    Comment model added and DB migrated

--- posts/models.py
+++ posts/models.py
@@ -46,3 +46,24 @@ class Post(models.Model):
     
     class Meta:
         ordering = ['-publication_date']
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments') 
+    publication_date = models.DateTimeField(editable=False)
+    content = models.TextField(max_length=512)
+    updated_on = models.DateTimeField()
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.author}-{self.post}"
+
+    def get_absolute_url(self):
+        return reverse('posts:post_detail', kwargs={'slug': self.post.slug})
+
+    class Meta:
+        ordering = ['-publication_date']
+    
