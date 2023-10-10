487e04ea1f682818d91a78f38baf1bb6f04c5e7a
FerranPalmaC <fpalmcom@sirt.com>
2023-10-10 16:31:15 +0200

    comment model tested

--- posts/tests/test_models.py
+++ posts/tests/test_models.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ValidationError
 from django.template.defaultfilters import slugify
 from django.test import Client, TestCase
 from django.utils import timezone
-from posts.tests.factories import PostFactory
+from posts.tests.factories import CommentFactory, PostFactory
 from members.tests.factories import CustomUserFactory
 
 class TestPostsAppModels(TestCase):
@@ -18,28 +18,35 @@ class TestPostsAppModels(TestCase):
         cls.client.login(username=cls.visitor_user.username, password=cls.visitor_user.password)
         # Set up posts
         cls.published_post = PostFactory(
+                author = cls.main_user,
                 creation_date = timezone.now(),
                 publication_date = timezone.now(),
                 updated_on = timezone.now(),
                 status=1)
 
         cls.drafted_post= PostFactory(
+                author = cls.main_user,
                 creation_date=timezone.now() - datetime.timedelta(days=2), 
                 publication_date=timezone.now() - datetime.timedelta(days=1), 
                 updated_on = timezone.now() + datetime.timedelta(days=3)
                 )
         cls.edited_post = PostFactory(
+                author = cls.main_user,
                 creation_date=timezone.now() - datetime.timedelta(days=2), 
                 publication_date=timezone.now() - datetime.timedelta(days=1), 
                 updated_on = timezone.now() + datetime.timedelta(days=3),
                 status=1
                 )
+        # Set up comments
+        cls.comment_post = CommentFactory(
+                author=cls.visitor_user,
+                post=cls.published_post
+                )
 
 class TestPostModel(TestPostsAppModels):
 
     def test_str_method(self):
-        post = PostFactory() 
-        self.assertEqual(post.title, str(post))
+        self.assertEqual(self.published_post.title, str(self.published_post))
 
     def test_has_been_edited(self):
         self.assertTrue(self.edited_post.has_been_edited())
@@ -49,12 +56,15 @@ class TestPostModel(TestPostsAppModels):
 
     def test_two_posts_with_same_title_fail(self):
         with self.assertRaises(ValidationError):
-            repeated_post = PostFactory(title=self.published_post.title)
+            _ = PostFactory(title=self.published_post.title)
 
     def test_slug_automatically_created(self):
         title = "This is a test title"
         auto_slug_post = PostFactory(title=title)
         self.assertEqual(slugify(title), auto_slug_post.slug)
 
-    
-       
+   
+class TestCommentModel(TestPostsAppModels):
+      
+    def test_str_method(self):
+        self.assertEqual(f"{self.comment_post.author.username} -- {self.comment_post.author.email}-{self.comment_post.post.title}", str(self.comment_post))
